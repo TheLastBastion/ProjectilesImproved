b1c5ab711f9f596d21e18179b161af05201a6327
Gauge <Gauger16@gmail.com>
2018-10-21 17:49:38 -0700

    yep

--- Effects/Explosive.cs
+++ Effects/Explosive.cs
@@ -188,14 +188,13 @@ namespace ProjectilesImproved.Effects
             //}
 
 
-            int value = 255 * (entity.Rays.Count / ExplosionRays[0].Length);
+            int value = 255 * (entity.Rays.Count / 20);
             MyVisualScriptLogicProvider.AddGPS("", "", bounds.Center, Color.FromNonPremultiplied(new Vector4(value, value, value, 255)), 5);
 
             if (entity.Rays.Count > 0)
             {
                 entities.Add(entity);
-                //int value = 255 * (entity.Rays.Count / ExplosionRays[0].Length);
-                //MyVisualScriptLogicProvider.AddGPS("", "", bounds.Center, Color.FromNonPremultiplied(new Vector4(value, value, value, 255)), 5);
+
             }
         }
 
