fb8fb9a651119a66fae44f03d1f326f09ffb791f
Gauge <Gauger16@gmail.com>
2018-11-02 15:35:39 -0700

    think i got it

--- Weapons/WeaponsBase.cs
+++ Weapons/WeaponsBase.cs
@@ -233,7 +233,7 @@ namespace ProjectilesImproved.Weapons
                 MatrixD muzzleMatrix = gun.GunBase.GetMuzzleWorldMatrix();
 
                 MatrixD positionMatrix = Matrix.CreateWorld(
-                    muzzleMatrix.Translation + muzzleMatrix.Forward * 0.25,
+                    muzzleMatrix.Translation + block.CubeGrid.Physics.LinearAcceleration,
                     gun.GunBase.GetDeviatedVector(gun.GunBase.DeviateAngle, muzzleMatrix.Forward),
                     muzzleMatrix.Up);
 
